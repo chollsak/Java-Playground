7d03a9978887c40857d1c0e86699a35ce1aa4627
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2025-02-12 00:52:32 +0700

    update

--- source/Display.java
+++ source/Display.java
@@ -1,7 +1,12 @@
 public class Display {
+
+    public static void Ronaldo(String args){
+        System.out.println(args);
+    }
     public static void main(String[] args) {
         System.out.println("Display");
         System.out.println("Java Playground");
-        // 
+        Ronaldo("Ronaldo");
+    
     }
 }
