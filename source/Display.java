205a7b88fbe4e4ac5eda289916a4750ac23165bc
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2025-02-13 00:23:41 +0700

    kaikai

--- source/Display.java
+++ source/Display.java
@@ -1,12 +1,28 @@
 public class Display {
 
-    public static void Ronaldo(String args){
-        System.out.println(args);
-    }
     public static void main(String[] args) {
         System.out.println("Display");
         System.out.println("Java Playground");
-        Ronaldo("Ronaldo");
-    
+
+        int number = 10;
+        final int constantNumber = 120;
+        final String hello = "kai kai";
+        System.out.println(hello);
+        System.out.println("Number is "+number);
+        System.out.println(constantNumber + number);
+
+        double numDouble = 10.62232131;
+        int numInt = (int) numDouble;
+        System.out.println(numInt);
+
+        String a = "abc"; //class String 
+        String b = "120";
+        Double c;
+
+        c = Double.parseDouble(b) + 10;
+        boolean result = c instanceof Double;
+
+        System.out.println(a + "def " +c+b+result);
+
     }
 }
---
+++ source/Input.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)){
+            
+            //Scanner sc = new Scanner(System.in); // object scanner
+            System.out.print("Enter your name: ");
+            String name = sc.nextLine(); // input string all line, next() is read only first input after blank " "
+
+            System.out.print("Enter your money: ");
+            Integer money = sc.nextInt();
+
+            int a = 5;
+            System.out.println("My name is "+name);
+            System.out.println("Your money total is " + ++money +" "+ --a);
+        } 
+
+    }
+}
