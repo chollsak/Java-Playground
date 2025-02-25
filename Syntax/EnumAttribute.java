9998a757b28e26c303d10946fad0240b3f27a234
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2025-02-25 17:38:48 +0700

    feat: Update something

--- Syntax/EnumAttribute.java
+++ Syntax/EnumAttribute.java
@@ -1,18 +1,18 @@
 enum Grade {
     A(4.0, "Very Good"),B(3.0, "Good Job"),C(2.0, "Not Bad"),D(1.0, "Keep it up!"),F(0.0, "Not today T-T");
 
-    private Grade(double p, String comment) {
+    private Grade(double p, String cm){
         this.point = p;
-        this.description = comment;
+        this.comment = cm;
     }
-    
+
     public final double point;
-    public final String description;
+    public final String comment;
 }
 
 public class EnumAttribute {
     public static void main(String[] args) {
         Grade g = Grade.A;
-        System.err.println(g.point + ", " + g.description);
+        System.out.println(g.point + " | " + g.comment + " | " + g);
     }
 }
