9909eab30ca57d4d4be50855a464de073c64b37d
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2025-02-26 15:37:36 +0700

    feat: update files

--- Syntax/ArrayLists.java
+++ Syntax/ArrayLists.java
@@ -4,29 +4,28 @@ import java.util.Scanner;
 public class ArrayLists {
     public static void main(String[] args) {
         ArrayList<String> data = new ArrayList<>();
+        Scanner kb = new Scanner(System.in);
         OUTER:
         while (true) {
-            Scanner kb = new Scanner(System.in);
             System.out.print("Please enter something to add in ArrayList: ");
-            String res = kb.next();
+            String res = kb.nextLine();
             switch (res) {
                 case "." -> {
                     break OUTER;
                 }
                 case "-" -> {
-                    if(data.isEmpty()){
+                    if (data.isEmpty()) {
                         System.out.println("Cannot delete more");
-                    }else{
-                        data.remove(data.size()-1);
+                    } else {
+                        data.remove(data.size() - 1);
                         System.out.println(data);
                     }
                 }
                 default -> data.add(res);
             }
-
         }
-
-        System.out.println("Thank you, for your attentions!!");
+        kb.close();
+        System.out.println("Thank you, for your attention!!");
         System.out.println("Contents of the ArrayList: " + data);
     }
 }
