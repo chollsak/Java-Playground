281d899e10aa5b0de0a38bb8627d8ed9efdf5cdb
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2025-04-04 03:55:50 +0700

    feat: Learning new OOP with Java

---
+++ ObjectOrientedProgramming/Class&Object/Employee.java
@@ -0,0 +1,6 @@
+public class Employee {
+    // Attribute
+    public String id;
+    private String name;
+    private Double salary;
+}
---
+++ ObjectOrientedProgramming/Class&Object/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();   // Create Object
+        e1.id = "65010195";
+        System.out.println("E1 id: " + e1.id);
+
+    }
+}
