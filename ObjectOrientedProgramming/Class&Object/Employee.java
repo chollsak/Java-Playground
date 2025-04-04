ab6ba3fc4c892dc8580a646d526db7b363050706
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2025-04-05 01:09:13 +0700

    feat: Getter Setter

--- ObjectOrientedProgramming/Class&Object/Employee.java
+++ ObjectOrientedProgramming/Class&Object/Employee.java
@@ -1,6 +1,47 @@
 public class Employee {
     // Attribute
-    public String id;
+    private String id;
     private String name;
     private Double salary;
+
+    // Method
+    // setter
+    public void setID(String id){
+        this.id = id;
+        System.out.println("Employee ID is setted -> " + id);
+    }
+
+    public void setName(String name){
+        this.name = name;
+        System.out.println("Employee name is setted -> " + name);
+    }
+
+    public void setSalary(Double salary){
+        this.salary = salary;
+        System.out.println("Employee salary is setted -> " + salary);
+    }
+
+    //getter
+    public String getID(){
+        System.out.println(this.id);
+        return this.id;
+    }
+
+    public String getName(){
+        System.out.println(this.name);
+        return this.name;
+    }
+
+    public Double getSalary(){
+        System.out.println(this.salary);
+        return this.salary;
+    }
+
+    public void showData(){
+        System.out.println("Employee ID: " + this.id);
+        System.out.println("Name: " + this.name);
+        System.out.println("Salary: " + this.salary);
+
+    }
+
 }
--- ObjectOrientedProgramming/Class&Object/main.java
+++ ObjectOrientedProgramming/Class&Object/main.java
@@ -2,8 +2,22 @@ public class main {
 
     public static void main(String[] args) {
         Employee e1 = new Employee();   // Create Object
-        e1.id = "65010195";
-        System.out.println("E1 id: " + e1.id);
+        e1.setID("65010195");
+        e1.setName("Chollasak Anuwareepong");
+        e1.setSalary(6000.0);
+        System.out.println("----------------");
+        e1.showData();
 
+        System.out.println(" ");
+
+        Employee e2 = new Employee();
+        e2.setID("65040254");
+        e2.setName("Marcus Rashford");
+        e2.setSalary(4239.0);
+        System.out.println("----------------");
+        e2.showData();
+
+
+        
     }
 }
