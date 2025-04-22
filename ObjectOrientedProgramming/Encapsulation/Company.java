105f474459adf6cb9db446bdfbfd463496c495b9
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2025-04-22 11:07:08 +0700

    Update: Longtime no see! Java

---
+++ ObjectOrientedProgramming/Encapsulation/Company.java
@@ -0,0 +1,4 @@
+public class Company {
+    static String name = "Traleleo Tralala";
+    static String createAt = "19-12-2020";
+}
---
+++ ObjectOrientedProgramming/Encapsulation/main.java
@@ -0,0 +1,8 @@
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Name:" + Company.createAt);
+        System.out.println("Created at: " + Company.createAt);
+
+    }
+}
