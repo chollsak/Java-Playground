7f9d59e72cc7adf974e88875990fff23d5751ac9
Chollasak Anuwareepong <chollsak@Chollasaks-MacBook-Air.local>
2025-04-23 16:14:17 +0700

    Update: Hello

--- SpringBoot-SimpleAPI/crud-api/src/main/java/com/chollsak/crud_api/controllers/UserController.java
+++ SpringBoot-SimpleAPI/crud-api/src/main/java/com/chollsak/crud_api/controllers/UserController.java
@@ -1,26 +1,23 @@
 package com.chollsak.crud_api.controllers;
-
+import com.chollsak.crud_api.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    @GetMapping("users")
-    public String getUsers(){
-        return "Chollsak, Nitcharat, Marcus Rashford";
-    }
-
-    @GetMapping("about")
-    public String showAbout(){
-        return "This is about";
-    }
-
-    @GetMapping("starter")
-    public Integer showNumber(){
-        return 1234;
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        List<User> data = new ArrayList<User>();
+        data.add(new User("Chollasak", "Anuwareepong"));
+        data.add(new User("Nitcharat", "Thattium"));
+        data.add(new User("Kidsada", "Raksakul"));
+        return data;
     }
 
 }
